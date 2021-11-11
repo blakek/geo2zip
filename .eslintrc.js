8dd92cd367908ae5ecf9b3c12692295dad5c8590
Blake Knight <bknight@gsandf.com>
2021-11-11 11:22:10 -0600

    :sparkles: migrate project to TypeScript

---
+++ .eslintrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+  extends: [require.resolve('amper-scripts/config/eslint')],
+  root: true
+};
---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import sphereKnn from 'sphere-knn';
+import {
+  GeolocationInput,
+  standardizeGeolocation
+} from 'standardize-geolocation';
+import { ZIPCode } from 'us-zips';
+import usZips, { ZIPCodeList } from 'us-zips/array';
+
+export interface Options {
+  limit: number;
+}
+
+export const defaultOptions: Options = { limit: 1 };
+
+// Start building lookup when module imported
+const lookup = sphereKnn(usZips);
+
+// Don't recreate this function on every run
+const pluckZipCode = (obj: { zipCode: string }) => obj.zipCode;
+
+export function geo2zip(
+  location: GeolocationInput,
+  extraOptions: Partial<Options> = {}
+): Promise<ZIPCode[]> {
+  const options = { ...defaultOptions, ...extraOptions };
+  const { latitude, longitude } = standardizeGeolocation(location);
+
+  return new Promise(resolve => {
+    const results = lookup(latitude, longitude, options.limit) as ZIPCodeList;
+
+    resolve(results.map(pluckZipCode));
+  });
+}
+
+export default geo2zip;
